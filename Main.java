be68b8aac2eef532c98ec42efe2fe8293611938d
Alfina Nisa A <alfinanisa29@gmail.com>
2023-05-11 16:57:49 +0700

    utspertama

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
     public static void main(String[] args) {
+    Mhs mhs = new(username:"ini_username", password:"ini_password");
+
+    mhs.setNama(nama:"Alfina Nisa");
+    mhs.setNim(nim:"G111190047");
+    mhs.setSmt(smt:3);
+
+    System.out.println("Nama Mhs: " +mhs.getNama());
+    System.out.println("Nim Mhs: " +mhs.getNim());
+    System.out.println("Smt Mhs: " +mhs.getSmt());
+    System.out.println("Username: " +mhs.getUsername());
+    System.out.println("Password: " +mhs.getPassword());
+
+
 
     }
 }
--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,19 @@
 package pengguna;
 
 class Mhs {
-    Mhs () {
+    String.nama;
+    String.nim;
+    int.smt;
 
+    void setNama(String arg_nama){
+        this.nama = arg_nama;
+    }
+
+    void setNim(String arg_nim){
+        this.nim = arg_nim;
+    }
+
+    void setSmt(int arg_smt){
+        this.smt = arg_smt;
     }
 }
