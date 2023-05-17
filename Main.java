226676b04c1eca70f9ca37598fbbd479d4ec66bd
Alfina  Nisa A <132651100+AlfinaNisaA@users.noreply.github.com>
2023-05-17 17:29:41 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
-    Mhs mhs = new(username:"ini_username", password:"ini_password");
 
-    mhs.setNama(nama:"Alfina Nisa");
-    mhs.setNim(nim:"G111190047");
-    mhs.setSmt(smt:3);
+        Mhs mhs = new Mhs("Ini_Username", "Ini_Password", "Alfina Nisa A", "G.111.19.0047", 8);
 
-    System.out.println("Nama Mhs: " +mhs.getNama());
-    System.out.println("Nim Mhs: " +mhs.getNim());
-    System.out.println("Smt Mhs: " +mhs.getSmt());
-    System.out.println("Username: " +mhs.getUsername());
-    System.out.println("Password: " +mhs.getPassword());
+        // mhs.setNama("Alfina Nisa A");
+        // mhs.setNim("G.111.19.0047");
+        // mhs.setSmt(8);
 
-
+        System.out.println("Nama Mhs : " + mhs.getNama());
+        System.out.println("Nim Mhs : " + mhs.getNim());
+        System.out.println("Smt Mhs : " + mhs.getSmt());
+        System.out.println("Username : " + mhs.getUsername());
+        System.out.println("Password : " + mhs.getPassword());
 
     }
 }
